70f647d96d3025029ba2351620b73383979b677e
Abdullah Al Muzaki <69757282+aamuzakii@users.noreply.github.com>
2021-02-20 16:59:58 +0800

    Create openpyxl_to_extract_plastick_from_SO.py

---
+++ openpyxl_to_extract_plastick_from_SO.py
@@ -0,0 +1,17 @@
+# Python Openpyexcel Cheatsheet
+# $ pip install openpyxl
+
+import openpyxl
+import os
+
+from openpyxl.utils.dataframe import dataframe_to_rows
+
+
+
+os.chdir(r'C:\Users\vivo\Downloads')
+
+wb = openpyxl.load_workbook('Order 21.02.19  - 1 Shopee (POP) REG.xlsx')
+
+sheet = wb['IDOS002-Shopee 1-10']
+
+ws = wb.active
