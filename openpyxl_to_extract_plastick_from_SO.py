021e616beabd243ff121bca9f2502c251aacd826
Abdullah Al Muzaki <69757282+aamuzakii@users.noreply.github.com>
2021-02-20 17:01:39 +0800

    Update openpyxl_to_extract_plastick_from_SO.py

--- openpyxl_to_extract_plastick_from_SO.py
+++ openpyxl_to_extract_plastick_from_SO.py
@@ -3,6 +3,7 @@
 
 import openpyxl
 import os
+import pandas as pd
 
 from openpyxl.utils.dataframe import dataframe_to_rows
 
@@ -14,4 +15,6 @@ wb = openpyxl.load_workbook('Order 21.02.19  - 1 Shopee (POP) REG.xlsx')
 
 sheet = wb['IDOS002-Shopee 1-10']
 
-ws = wb.active
+# ws = wb.active
+
+df = pd.DataFrame(ws.values)
