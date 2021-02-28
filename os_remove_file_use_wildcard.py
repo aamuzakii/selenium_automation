374aaad645da98d33b054e60de6bdf2144278e1f
Abdullah Al Muzaki <69757282+aamuzakii@users.noreply.github.com>
2021-02-28 17:06:13 +0700

    Update and rename os_remove_file_use_wildcard.py to senin-1-mar.py

--- os_remove_file_use_wildcard.py
+++
@@ -1,8 +0,0 @@
-import os
-import glob
-fileList = glob.glob(r'C:\Users\user\Documents\Abi\delete\*(*).accdb', recursive=True)
-for file in fileList:
-    try:
-        os.remove(filePath)
-    except OSError:
-        print("Error while deleting file")
---
+++ senin-1-mar.py
@@ -0,0 +1,25 @@
+import os
+import glob
+fileList = glob.glob(r'C:\Users\user\Documents\Abi\delete\*(*).accdb', recursive=True)
+for file in fileList:
+    try:
+        os.remove(filePath)
+    except OSError:
+        print("Error while deleting file")
+        
+        
+       
+    
+    driver.close()
+    driver.execute_script("window.open('');")
+    
+    
+    
+if os.path.isfile('save.txt'):
+    smwa pusat directory atau path harus ganti ke tiap komputer karena browser gabisa auto download ke PC lain   
+    
+    algo:
+        1. OS clear dulu yang (1) dan clear IMEI record
+        2. buka tiap tab, for each tab:
+            1. get OPDDFH dan nama file spt biasa
+            2. chek, jika di OS belum ada yang spt di tab(minus extension) maka click smwa .xlsx
